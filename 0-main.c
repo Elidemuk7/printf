538256e7f94fe91d5d26e2ece9e27359a8bfc8e0
Elidemuk7 <elijahmukoro7@gmail.com>
2023-06-29 08:13:22 -0700

    tsk0

--- 0-main.c
+++ 0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -7,9 +8,19 @@
  */
 int main(void)
 {
+	int ret_val1, ret_val2;
+/*	char letter = 'E';*/
 	char str[] = "Elijah and Dimpho";
 
-	_printf("the team members are: %c\n", str);
+	_puts_("\n****Using the new _printf****\n");
+	ret_val1 = _printf("\nthe team members are: %s\n", str);
+	_printf("the team members are: %s\n", str);
+	printf("the return value is: %d\n", ret_val1);
+
+	_puts_("\n****Using the old printf****\n");
+	ret_val2 = printf("\nthe team members are: %s\n", str);
+	printf("the team members are: %s\n", str);
+	printf("the return value is: %d\n", ret_val2);
 
 	return (0);
 }
